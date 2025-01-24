f1eab0f3d4cc1dbfdacab67308b1daf2cb45cbff
Ddos-0182 <telegrambot0001@outlook.com>
2025-01-24 15:17:01 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -5,10 +5,10 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7730439450:AAEmmtfG59WL4Sbr4ERy8iHYkZjxY8B2CRs')
+bot = telebot.TeleBot('8004531749:AAE1b_YeYV8IeNmFxXXyjLwTRt_la7jow8o')
 
 # Admin user IDs
-admin_id = {"7083378335", "1436507974"}
+admin_id = {"7083378335", "6925561335"}
 
 # File to store allowed user IDs
 USER_FILE = "users.txt"
@@ -148,7 +148,7 @@ def add_user(message):
         else:
             response = "Please specify a user ID and the duration (e.g., 1hour, 2days, 3weeks, 4months) to add ."
     else:
-        response = "Mood ni hai abhi pelhe purchase kar isse:- @Tg_maggi."
+        response = "Mood ni hai abhi pelhe purchase kar isse:- @LANKAxARINDAM."
 
     bot.reply_to(message , response)
 
@@ -184,7 +184,7 @@ def remove_user(message):
             response = '''Please Specify A User ID to Remove. 
  Usage: /remove <userid>'''
     else:
-        response = "Purchase karle bsdk:- @Tg_maggi ."
+        response = "Purchase karle bsdk:- @LANKAxARINDAM ."
 
     bot.reply_to(message, response)
     
@@ -352,8 +352,8 @@ def show_help(message):
 👀 To See Admin Commands:
 🤖 /admincmd : Shows All Admin Commands.
 
-Buy From :- @Tg_maggi
-Official Channel :- https://t.me/niggacheatss
+Buy From :- @LANKAxARINDAM
+Official Channel :- https://t.me/vipddosshinchan
 '''
     for handler in bot.message_handlers:
         if hasattr(handler, 'commands'):
@@ -370,7 +370,7 @@ def welcome_start(message):
     user_name = message.from_user.first_name
     response = f'''SERVER ke LODE pe aapka swagat hai, {user_name}! Sabse acche se bgmi ki maa behen yahi hack karta hai. Kharidne ke liye Kira se sampark karein.
 🤗Try To Run This Command : /help 
-💵BUY :- @Tg_maggi'''
+💵BUY :- @LANKAxARINDAM'''
     bot.reply_to(message, response)
 
 @bot.message_handler(commands=['rules'])
@@ -380,7 +380,7 @@ def welcome_rules(message):
 
 1. Dont Run Too Many Attacks !! Cause A Ban From Bot
 2. Dont Run 2 Attacks At Same Time Becz If U Then U Got Banned From Bot.
-3. MAKE SURE YOU JOINED https://t.me/niggacheatss OTHERWISE NOT WORK
+3. MAKE SURE YOU JOINED https://t.me/vipddosshinchan OTHERWISE NOT WORK
 4. We Daily Checks The Logs So Follow these rules to avoid Ban!!'''
     bot.reply_to(message, response)
 
