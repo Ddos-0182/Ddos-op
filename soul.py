622e4210ee44c4bf173827c06ceab3e45bcf7d9a
Ddos-0182 <telegrambot0001@outlook.com>
2025-02-09 22:59:11 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -5,7 +5,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7730439450:AAEmmtfG59WL4Sbr4ERy8iHYkZjxY8B2CRs')
+bot = telebot.TeleBot('7420677434:AAE-n0u9rdYKO6gFPYl5ZPnIYxF73zvjBGI')
 
 # Admin user IDs
 admin_id = {"7083378335", "7116439045"}
@@ -148,7 +148,7 @@ def add_user(message):
         else:
             response = "Please specify a user ID and the duration (e.g., 1hour, 2days, 3weeks, 4months) to add ."
     else:
-        response = "Mood ni hai abhi pelhe purchase kar isse:- @xD_Rajveer."
+        response = "Mood ni hai abhi pelhe purchase kar isse:- @TREXVIVEK."
 
     bot.reply_to(message , response)
 
@@ -184,7 +184,7 @@ def remove_user(message):
             response = '''Please Specify A User ID to Remove. 
  Usage: /remove <userid>'''
     else:
-        response = "Purchase karle bsdk:- @xD_Rajveer ."
+        response = "Purchase karle bsdk:- @TREXVIVEK ."
 
     bot.reply_to(message, response)
     
@@ -352,7 +352,7 @@ def show_help(message):
 👀 To See Admin Commands:
 🤖 /admincmd : Shows All Admin Commands.
 
-Buy From :- @xD_Rajveer
+Buy From :- @TREXVIVEK
 Official Channel :- na
 '''
     for handler in bot.message_handlers:
@@ -370,7 +370,7 @@ def welcome_start(message):
     user_name = message.from_user.first_name
     response = f'''SERVER HACK pe aapka swagat hai, {user_name}! Sabse acche se bgmi ke server yahi hack karta hai. Kharidne ke liye Kira se sampark karein.
 🤗Try To Run This Command : /help 
-💵BUY :- @xD_Rajveer'''
+💵BUY :- @TREXVIVEK'''
     bot.reply_to(message),
 @bot.message_handler(commands=['rules'])
 def welcome_rules(message):
