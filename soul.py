95e60acc629910f8257b4bffb930e65787f080fa
Ddos-0182 <telegrambot0001@outlook.com>
2025-02-10 10:35:15 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -5,7 +5,7 @@ import os
 from keep_alive import keep_alive
 keep_alive()
 # Insert your Telegram bot token here
-bot = telebot.TeleBot('7420677434:AAE-n0u9rdYKO6gFPYl5ZPnIYxF73zvjBGI')
+bot = telebot.TeleBot('7656471871:AAEeo7xzHCQ4VkYEAQaOm6iFQV0_RksRcsQ')
 
 # Admin user IDs
 admin_id = {"7083378335", "7116439045"}
